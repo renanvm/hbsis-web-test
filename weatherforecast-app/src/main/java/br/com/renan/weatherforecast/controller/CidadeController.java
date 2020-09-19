bcdeaa326a25b0168bcb4b06d6f01bad0b89e20c
Renan Martins <renantins@outlook.com>
2020-09-19 11:35:29 -0300

    Cidade REST Endpoints

--- weatherforecast-app/src/main/java/br/com/renan/weatherforecast/controller/CidadeController.java
+++ weatherforecast-app/src/main/java/br/com/renan/weatherforecast/controller/CidadeController.java
@@ -1,12 +1,64 @@
 package br.com.renan.weatherforecast.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.renan.weatherforecast.model.Cidade;
+import br.com.renan.weatherforecast.repository.CidadeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/api/cidades")
 @CrossOrigin()
 public class CidadeController {
+
+    private CidadeRepository cidadeRepository;
+
+    public CidadeController(CidadeRepository cidadeRepository) {
+        this.cidadeRepository = cidadeRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createCidade(@RequestBody Cidade cidade) throws URISyntaxException {
+            Cidade result = cidadeRepository.save(cidade);
+            return ResponseEntity.created(new URI("/api/cidades/" + result.getId()))
+                    .body(result);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateCidade(@RequestBody Cidade cidade) {
+        if (cidade.getId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Campo Id vazio");
+        }
+        Cidade result = cidadeRepository.save(cidade);
+        return ResponseEntity.ok()
+                .body(result);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllCidades() {
+        return ResponseEntity.ok().body(cidadeRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCidade(@PathVariable Long id) {
+        if (cidadeRepository.findById(id).isPresent()) {
+            Cidade cidade = cidadeRepository.findById(id).get();
+            return ResponseEntity.ok().body(cidade);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCidade(@PathVariable Long id) {
+        if (cidadeRepository.findById(id).isPresent()) {
+            cidadeRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
