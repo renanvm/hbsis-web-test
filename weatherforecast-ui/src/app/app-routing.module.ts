a760b4497170a1e441afc9ae6c8bd3010048aee9
Renan Martins <renantins@outlook.com>
2020-09-19 16:13:20 -0300

    Rota para ListCidades

--- weatherforecast-ui/src/app/app-routing.module.ts
+++ weatherforecast-ui/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {CreateCidadeComponent} from './create-cidade/create-cidade.component';
+import {ListCidadesComponent} from './list-cidades/list-cidades.component';
 
 const routes: Routes = [
-  { path: 'create-cidade', component: CreateCidadeComponent }];
+  {path: 'create-cidade', component: CreateCidadeComponent},
+  {path: 'list-cidades', component: ListCidadesComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
