0e09f2fc8cf1099b4bb9e24556cfebb970819c8f
Renan Martins <renantins@outlook.com>
2020-09-19 09:40:45 -0300

    Projeto inicial Spring Boot

---
+++ weatherforecast-app/src/test/java/br/com/renan/weatherforecast/WeatherforecastApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.renan.weatherforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherforecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
