46a19909c66a1b659ca0d4bf7fd952cba8affaff
Renan Martins <renantins@outlook.com>
2020-09-19 10:46:01 -0300

    Cidade Controller

---
+++ weatherforecast-app/src/main/java/br/com/renan/weatherforecast/controller/CidadeController.java
@@ -0,0 +1,12 @@
+package br.com.renan.weatherforecast.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cidades")
+@CrossOrigin()
+public class CidadeController {
+
+}
