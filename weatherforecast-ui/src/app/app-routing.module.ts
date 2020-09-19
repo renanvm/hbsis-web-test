e2f06721df642b95862092d54c1c92e0af28a0b7
Renan Martins <renantins@outlook.com>
2020-09-19 15:34:18 -0300

    Rota para Component CreateCidade

--- weatherforecast-ui/src/app/app-routing.module.ts
+++ weatherforecast-ui/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CreateCidadeComponent} from './create-cidade/create-cidade.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create-cidade', component: CreateCidadeComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
