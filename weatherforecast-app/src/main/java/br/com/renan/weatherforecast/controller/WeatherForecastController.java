fff4ad63c168cdb21ce4f387e5baa51924943d8f
Renan Martins <renantins@outlook.com>
2020-09-20 16:07:17 -0300

    Endpoint para previsao climatica dos proximos dias

--- weatherforecast-app/src/main/java/br/com/renan/weatherforecast/controller/WeatherForecastController.java
+++ weatherforecast-app/src/main/java/br/com/renan/weatherforecast/controller/WeatherForecastController.java
@@ -1,14 +1,20 @@
 package br.com.renan.weatherforecast.controller;
 
+import br.com.renan.weatherforecast.model.Previsao;
 import br.com.renan.weatherforecast.service.WeatherForecastService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/weather")
+@RequestMapping("/api/forecast")
 @CrossOrigin()
 public class WeatherForecastController {
 
@@ -16,8 +22,14 @@ public class WeatherForecastController {
     private String apiKey;
     @Value("${openweather.api.url}")
     private String apiUrl;
+    @Value("${openweather.api.url.onecall}")
+    private String apiUrlOneCall;
     @Value("${openweather.api.lang}")
     private String apiLang;
+    @Value("${openweather.api.units}")
+    private String units;
+    @Value("${openweather.api.exclude}")
+    private String exclude;
     private RestTemplate restTemplate = new RestTemplate();
     private WeatherForecastService weatherForecastService;
 
@@ -25,9 +37,9 @@ public class WeatherForecastController {
         this.weatherForecastService = weatherForecastService;
     }
 
-    @GetMapping("/{cidade}")
+    @GetMapping("/check/{cidade}")
     public ResponseEntity<?> checkCidadeIsValid(@PathVariable String cidade) {
-        String requestUrl = apiUrl + cidade + apiKey + apiLang;
+        String requestUrl = apiUrl + cidade + apiKey;
         try {
             int statusCode = restTemplate.getForEntity(requestUrl, String.class).getStatusCode().value();
             return ResponseEntity.status(statusCode).build();
@@ -35,5 +47,14 @@ public class WeatherForecastController {
             int statusCode = exception.getStatusCode().value();
             return ResponseEntity.status(statusCode).build();
         }
+    }
+
+    @GetMapping("/{cidade}")
+    public ResponseEntity<List<Previsao>> getForecastWeather(@PathVariable String cidade) throws JsonProcessingException {
+        String requestCoordinates = apiUrl + cidade + apiKey + apiLang + units;
+        Map coordinates = this.weatherForecastService.getCoordenadasCidade(restTemplate.getForObject(requestCoordinates, String.class).toString());
+        String urlOneCall = apiUrlOneCall+"lat="+coordinates.get("latitude")+"&lon="+coordinates.get("longitude")+apiLang+exclude+units+apiKey;
+        List<Previsao> newForecastJson = this.weatherForecastService.handleForecastJson(restTemplate.getForEntity(urlOneCall, String.class).getBody());
+        return ResponseEntity.ok().body(newForecastJson);
     }
 }
