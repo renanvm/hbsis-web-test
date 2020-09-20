cce8c0f199b54b58a56f66d9aff320cce5254a38
Renan Martins <renantins@outlook.com>
2020-09-20 12:42:09 -0300

    Metodo para obter coordenadas

--- weatherforecast-app/src/main/java/br/com/renan/weatherforecast/service/WeatherForecastService.java
+++ weatherforecast-app/src/main/java/br/com/renan/weatherforecast/service/WeatherForecastService.java
@@ -1,10 +1,29 @@
 package br.com.renan.weatherforecast.service;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class WeatherForecastService {
 
+    public Map getCoordenadasCidade(String result) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map coordenadas = new HashMap<String, String>();
+        try {
+            JsonNode jsonNode = objectMapper.readValue(result, JsonNode.class);
+            JsonNode coordNode = jsonNode.get("coord");
+            coordenadas.put("longitude", coordNode.get("lon").asText());
+            coordenadas.put("latitude", coordNode.get("lat").asText());
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return coordenadas;
+    }
 }
